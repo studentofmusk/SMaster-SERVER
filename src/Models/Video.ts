3017890cabb9f8aa9d1b9a33e8bcec90eeebf29a
mandalotech <farooqvrofficial@gmail.com>
2025-02-02 13:50:19 +0530

    Video Model created

--- src/Models/Video.ts
+++ src/Models/Video.ts
@@ -9,7 +9,28 @@ export interface IVideo extends Document{
 }
 
 const videoSchema = new Schema<IVideo>({
-    
+    title:{
+        type: String,
+        unique: true,
+        required: [true, "Video Title is required!"] 
+    },
+    url:{
+        type: String,
+        required: [true, "Video url is required!"]
+    },
+    thumbnail:{
+        type: String,
+        required: [true, "Thumbnail is required!"]
+    },
+    audio: {
+        type: String,
+        required: [true, "Audio is required!"]
+    },
+    action_id: {
+        type: Number,
+        required: [true, "Action ID required"],
+        default: -1
+    }    
 
 },{timestamps: true});
 
