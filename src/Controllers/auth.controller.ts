6c1d9cbebc888fa625b9bcb846cf659c0b4b6a8f
unknown <farooqvrofficial@gmail.com>
2025-02-07 21:37:21 +0530

    forgot and change password func created

--- src/Controllers/auth.controller.ts
+++ src/Controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import User, { Gender } from "../Models/User.js"
 import {Request, Response} from "express";
-import { emailValidator, signupValidator } from "../Utils/Validator.js";
+import { changePasswordValidator, emailValidator, signupValidator } from "../Utils/Validator.js";
 import { StatusCodes } from "http-status-codes";
 import bcryptjs from "bcryptjs";
 import { loginValidator } from '../Utils/Validator.js';
@@ -122,5 +122,86 @@ export const signupOtp = async(req: Request, res: Response): Promise<any> =>{
 
     } catch (error) {
         handleError(error, res, "Error in SEND OTP");
+    }
+}
+
+export const forgotPassword = async(req: Request, res: Response): Promise<any> =>{
+    try {
+        const validatedData = emailValidator.parse(req.body);
+
+        const USER = await User.findOne({email: validatedData.email});
+        if(!USER) return res.status(StatusCodes.NOT_FOUND).json({
+            success: false,
+            message: "User Not Found! Please check your email address."
+        });
+
+        
+        const payload = {
+            user_id: USER._id
+        }
+        const token = jwt.sign(payload, process.env.SECRETKEY!, {expiresIn: "15m"});
+
+        const sent = await sendMail({
+            from: "SMaster Security",
+            to: validatedData.email,
+            subject:"Forgot Password",
+            text: `Click here to change your password: http://${process.env.DOMAIN}/change-password?token=${token} \nThis Link will be valid until 15 Minutes. Please Don't share the link!`,
+            html: `
+            <h1>Change Password</h1>
+            <div>
+                 <a href="http://${process.env.DOMAIN}/change-password?token=${token}">Click here</a> to change your password.
+            </div>
+            <div>
+            This Link will be valid until <B>15 Minutes</B>. Please <span style="color:'red'">Don't share</span> the link!
+            </div>
+            `
+        });
+
+        if(!sent) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Unable to sent mail!"
+        });
+        console.log("Token:", token);
+
+        res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Mail sent to your email! please check your mail box."
+        });
+
+    } catch (error) {
+        handleError(error, res, "Error in FORGOT PASSWORD API");
+    }
+}
+
+export const changePassword = async(req: Request, res: Response): Promise<any> => {
+    try {
+        const validatedData = changePasswordValidator.parse(req.body);
+        
+        const data = jwt.verify(validatedData.token, process.env.SECRETKEY!);
+        if(!data || typeof data !== "object" || !data.user_id){
+            return res.status(StatusCodes.UNAUTHORIZED).json({
+                success: false,
+                message: "Invalid Token! Try again with valid token."
+            });
+        }
+
+        const USER = await User.findById(data.user_id);
+        if(!USER) return res.status(StatusCodes.NOT_FOUND).json({
+            success: false,
+            message: "Invalid User ID!"
+        });
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(validatedData.password, salt);
+
+        await USER.change_password(hash);
+
+        res.status(StatusCodes.OK).json({
+            success: true,
+            message: "Password Changed successfully!"
+        });
+
+    } catch (error) {
+        handleError(error, res, "Errorn in CHANGE PASSWORD API")
     }
 }
