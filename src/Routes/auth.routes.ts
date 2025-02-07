2df3f68059590871dec79d2bf92cec5b194d5c55
unknown <farooqvrofficial@gmail.com>
2025-02-07 21:39:37 +0530

    auth routes updated

--- src/Routes/auth.routes.ts
+++ src/Routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router} from "express";
-import { login, signup, signupOtp } from "../Controllers/auth.controller.js";
+import { changePassword, forgotPassword, login, signup, signupOtp } from "../Controllers/auth.controller.js";
 
 // Initialize route
 const auth_routes = Router();
@@ -7,5 +7,7 @@ const auth_routes = Router();
 auth_routes.post("/sign-up", signup);
 auth_routes.post("/log-in", login);
 auth_routes.post("/signup-otp", signupOtp);
+auth_routes.post("/forgot-password", forgotPassword);
+auth_routes.post("/change-password", changePassword);
 
 export default auth_routes;
