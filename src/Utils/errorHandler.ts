613e2606251f7bebbee4b0f9f65be3473bdd25f7
unknown <farooqvrofficial@gmail.com>
2025-02-07 21:40:17 +0530

    Error Handler updated

--- src/Utils/errorHandler.ts
+++ src/Utils/errorHandler.ts
@@ -1,8 +1,9 @@
 import { Response } from "express";
 import { ZodError } from "zod";
 import { StatusCodes } from "http-status-codes";
-import { logErrorToFile } from "./logger.js"; // Assuming you have a logger
-
+import { logErrorToFile } from "./logger.js"; 
+import jwt from "jsonwebtoken";
+const {TokenExpiredError} = jwt;
 export const handleError = (error: unknown, res: Response, customMessage: string) => {
     if (error instanceof ZodError) {
         return res.status(StatusCodes.BAD_REQUEST).json({
@@ -11,6 +12,13 @@ export const handleError = (error: unknown, res: Response, customMessage: string
             error: error.errors
         });
     }
+    else if (error instanceof TokenExpiredError){
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: "Token Expired!"
+        });
+    }
+    
 
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
         success: false,
