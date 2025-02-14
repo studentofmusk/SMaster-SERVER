a17c0ca97996f84a2bed20f889a1d038d74a2c9c
Farooq Khan <farooqvrofficial@gmail.com>
2025-02-14 13:06:44 +0530

    course_routes added

--- src/index.ts
+++ src/index.ts
@@ -15,10 +15,12 @@ import "./DB/conn.js"
 // Route Imports
 import auth_routes from "./Routes/auth.routes.js";
 import user_routes from "./Routes/user.routes.js";
+import course_routes from "./Routes/course.routes.js";
 
 //routes
 app.use("/api/auth", auth_routes);
 app.use("/api/user", user_routes)
+app.use("/api/course", course_routes)
 
 // Environment variables
 const PORT = process.env.PORT || 5000;
