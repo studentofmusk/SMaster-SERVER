885ac46eb25e02f7395de2287e4ddc37b26d74f4
Rahini4 <rahini452004@gmail.com>
2025-02-14 19:14:33 +0530

    create-lecture route created

--- src/Routes/course.routes.ts
+++ src/Routes/course.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { uploadVideo } from "../Utils/upload.js";
-import { create_video } from "../Controllers/course.controller.js";
+import { create_lecture, create_video, get_lectures, get_videos } from "../Controllers/course.controller.js";
 const course_routes = Router();
 
 course_routes.post("/create-video", uploadVideo, create_video);
+course_routes.post("/create-lecture", create_lecture);
+
+
+course_routes.get("/get-videos", get_videos)
+course_routes.get("/get-lectures", get_lectures)
 
 export default course_routes;
