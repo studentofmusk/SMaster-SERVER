0776a983bdca7a21493bb2a597ccaff945e16470
Farooq Khan <farooqvrofficial@gmail.com>
2025-02-14 08:32:14 +0530

    updated env

--- dist/DB/conn.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-mongoose.connect(process.env.DB)
-    .then(() => console.log("DB Connected!"))
-    .catch((err) => console.log(`DB Connection Failed!\n${err.message}`));
