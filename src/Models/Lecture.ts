b2c76d3298c41b197dc876922f9d40813ded1834
Rahini4 <rahini452004@gmail.com>
2025-02-14 13:08:04 +0530

    title added in schema

--- src/Models/Lecture.ts
+++ src/Models/Lecture.ts
@@ -1,10 +1,16 @@
 import mongoose, {Schema, Types, Document} from "mongoose";
 
 export interface ILecture extends Document{
+    title: string;
     video: Types.ObjectId;
 }
 
 const lectureSchema = new Schema<ILecture>({
+    title:{
+        type: String,
+        unique: true,
+        required: [true, "Video Title is required!"]
+    },
     video:{
         type: Schema.Types.ObjectId,
         ref:"Video",
